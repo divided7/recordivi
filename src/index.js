9a09d4a70672405d3df48625edc3d7a54e49b53d
Divyansh <divi.sunway@gmail.coom>
2020-04-29 16:26:11 +0530

    Excluding Dev Ops

--- src/index.js
+++ src/index.js
@@ -20,8 +20,6 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 
   mainWindow.maximize();
   mainWindow.show();
